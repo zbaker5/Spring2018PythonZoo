02817b83e7c220572ccd560dd0aa84c2aedc92f3
zbaker5 <zachary.baker057@gmail.com>
2018-04-25 14:41:17 +0000

    Models Work

--- pythonzoo/zoo/admin.py
+++ pythonzoo/zoo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Zoo, Exhibit, Animal
+
 # Register your models here.
+
+
+class ZooAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+admin.site.register(Zoo, ZooAdmin)
+
+class ExhibitAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+admin.site.register(Exhibit, ExhibitAdmin)
+
+class AnimalAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+admin.site.register(Animal, AnimalAdmin)	
--- pythonzoo/zoo/models.py
+++ pythonzoo/zoo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Zoo(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter a Zoo Name")
+	
+	def __str__(self):
+		return self.name
+	
+class Exhibit(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter a Exhibit Name")
+	zoo = models.ForeignKey('Zoo', on_delete=models.SET_NULL, null=True)
+	
+	def __str__(self):
+		return self.name
+		
+class ExhibitNeighbors(models.Model):
+	#fromExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+	direction = models.CharField(max_length=200)
+	#toExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+	
+class Animal(models.Model):
+	name = models.CharField(max_length=200, help_text="Enter an Animal Name")
+	exhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+	
+	def __str__(self):
+		return self.name
