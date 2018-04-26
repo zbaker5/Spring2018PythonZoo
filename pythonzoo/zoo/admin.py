9f1b50fb9f52a3a2fed9affcb4807e5d360c0750
zbaker5 <zachary.baker057@gmail.com>
2018-04-26 14:15:00 +0000

    Added More Stuff

--- pythonzoo/zoo/admin.py
+++ pythonzoo/zoo/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
 
-from .models import Zoo, Exhibit, Animal
+from .models import Zoo, Exhibit, Animal, Neighbor
 
 # Register your models here.
 
 
 class ZooAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name')
+	list_display = ('name', 'id')
 
 admin.site.register(Zoo, ZooAdmin)
 
 class ExhibitAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name')
+	list_display = ('name', 'zoo', 'animals', 'id')
 
 admin.site.register(Exhibit, ExhibitAdmin)
 
 class AnimalAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name')
+	list_display = ('name', 'exhibit', 'zoo', 'id')
 
 admin.site.register(Animal, AnimalAdmin)	
+
+class NeighborAdmin(admin.ModelAdmin):
+	list_display = ('toExhibit', 'fromExhibit', 'direction', 'id')
+	
+admin.site.register(Neighbor, NeighborAdmin)
--- pythonzoo/zoo/models.py
+++ pythonzoo/zoo/models.py
@@ -11,18 +11,29 @@ class Zoo(models.Model):
 class Exhibit(models.Model):
 	name = models.CharField(max_length=200, help_text="Enter a Exhibit Name")
 	zoo = models.ForeignKey('Zoo', on_delete=models.SET_NULL, null=True)
+	animals = models.ForeignKey('Animal', on_delete=models.SET_NULL, null=True, related_name = 'animalAtExhibit', blank=True)
 	
 	def __str__(self):
 		return self.name
+		return self.zoo
+		return self.animal
 		
-class ExhibitNeighbors(models.Model):
-	#fromExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+class Neighbor(models.Model):
+	fromExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True, related_name = 'fromExhibit')
 	direction = models.CharField(max_length=200)
-	#toExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+	toExhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True, related_name = 'toExhibit')
+	
+	def __str__(self):
+		return str(self.fromExhibit)
+		return self.direction
+		return str(self.toExhibit)
 	
 class Animal(models.Model):
 	name = models.CharField(max_length=200, help_text="Enter an Animal Name")
-	exhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True)
+	exhibit = models.ForeignKey('Exhibit', on_delete=models.SET_NULL, null=True, related_name = 'Exhibit', blank=True)
+	zoo = models.ForeignKey('Zoo', on_delete=models.SET_NULL, null=True)
 	
 	def __str__(self):
 		return self.name
+		return self.zoo
+		return self.exhibit
